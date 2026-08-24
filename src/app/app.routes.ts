a0ae5d91c90b2e6bc1358db63ad542d26ca08084
Biel25 <gabrielsampaio009officer@gmail.com>
2026-08-24 11:56:10 -0300

    criação do componente login e dashboard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
 
+    {path:"", redirectTo:"login", pathMatch:"full"},
+    {path:"dashboard", component:Dashboard},
     {path:"login", component:Login}
 ];
