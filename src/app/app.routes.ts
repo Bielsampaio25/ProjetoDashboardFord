044e8dd012f90a2749c168236db740dd4f69bd4a
Biel25 <gabrielsampaio009officer@gmail.com>
2026-08-27 11:55:03 -0300

    criação do componente home, adição da pasta image no public e models.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
 
     {path:"", redirectTo:"login", pathMatch:"full"},
     {path:"dashboard", component:Dashboard},
-    {path:"login", component:Login}
+    {path:"login", component:Login},
+    {path:"home", component:Home}
 ];
