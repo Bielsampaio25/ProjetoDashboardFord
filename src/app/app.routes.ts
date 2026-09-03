b4faea50508b498a0d2fa9707e71bf12fe40f30f
Biel25 <gabrielsampaio009officer@gmail.com>
2026-09-03 11:56:38 -0300

    criação do componente menu, finalização da tela de login e da tela de home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Home } from './pages/home/home';
+import { Menu } from './componentes/menu/menu';
 
 export const routes: Routes = [
 
     {path:"", redirectTo:"login", pathMatch:"full"},
     {path:"dashboard", component:Dashboard},
     {path:"login", component:Login},
-    {path:"home", component:Home}
+    {path:"home", component:Home},
+    {path:"menu", component:Menu}
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Menu } from '../../componentes/menu/menu';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, Menu],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
