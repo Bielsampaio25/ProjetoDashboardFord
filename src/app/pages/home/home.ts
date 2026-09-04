8d6f076217cfac792dc238b996f62975d5dba916
Biel25 <gabrielsampaio009officer@gmail.com>
2026-09-04 11:34:29 -0300

    finalização da tela de home e criação do componente header para o cabeçalho.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from '../../componentes/menu/menu';
+import { Header } from "../../componentes/header/header";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
