28b62e4e25dbc55768d0450657e8da897d3e60b6
Biel25 <gabrielsampaio009officer@gmail.com>
2026-09-09 11:28:23 -0300

    construçaõ da camada de serviço e configuração da api.

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class Login {
+  
 }
---
+++ src/app/services/login-auth.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginAuth {
+    private apiUrl = 'http://localhost:3001/';
+
+    constructor(private http:HttpClient){}
+      
+    login(usuario:Pick<Usuario, 'nome' | 'senha'>):Observable<Usuario>{
+        return this.http.post<Usuario>(`${this.apiUrl}/login`, usuario);
+      }
+    
+}
